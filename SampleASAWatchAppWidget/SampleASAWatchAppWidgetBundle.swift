d74625b7cbcab93b334ce60d04ac5a29ae4f8e08
Ephrim <mirhpe17@gmail.com>
2025-12-04 19:23:19 +0530

    watch widget in progress

---
+++ SampleASAWatchAppWidget/SampleASAWatchAppWidgetBundle.swift
@@ -0,0 +1,17 @@
+//
+//  SampleASAWatchAppWidgetBundle.swift
+//  SampleASAWatchAppWidget
+//
+//  Created by ephrim.daniel on 04/12/25.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct SampleASAWatchAppWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        SampleASAWatchAppWidget()
+        SampleASAWatchAppWidgetControl()
+    }
+}
